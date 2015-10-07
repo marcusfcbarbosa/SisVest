b41e1921ada6306aa42347af16676194a6feaee6
unknown <mfcbarbosa@DSK-0362.eicon.com.br>
2015-10-07 16:37:51 -0300

    Definindo o controller do Curso como sendo a rota padrao

--- SisVest.WebUI/Global.asax.cs
+++ SisVest.WebUI/Global.asax.cs
@@ -27,7 +27,7 @@ namespace SisVest.WebUI
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Curso", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
