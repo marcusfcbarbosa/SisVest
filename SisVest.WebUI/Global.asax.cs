673d80c5b0f8c8cbb3274e628f88d4ef0cab7227
unknown <mfcbarbosa@DSK-0362.eicon.com.br>
2015-10-21 11:43:32 -0200

    inserindo um layout responsivo como Dashboard

--- SisVest.WebUI/Global.asax.cs
+++ SisVest.WebUI/Global.asax.cs
@@ -28,9 +28,8 @@ namespace SisVest.WebUI
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Vestibular", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
